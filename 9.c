1b9144271e6f29c60a6e3686a548bd1aebae1438
yashwanth8634 <yashwanthreddykoppula@gmail.com>
2024-11-30 00:09:42 +0530

    small mistakes

--- 9.c
+++ 9.c
@@ -3,12 +3,12 @@
 int main(){
     int n,i,j;
     printf("Enter n value\n");
-    scanf("%d",&n);
+    scanf("%d ",&n);
     for(i=1;i<=n-1;i++){
         for(j=1;j<=i;j++){
             printf("* ");
         }
-        printf("\n");
+        printf("\n ");
     }
     for(i=n;i>=1;i--){
         for(j=i;j>=1;j--){
